4c1b00d65bea962104c74ee678b954351735c45c
karim-hafez77 <karimmagdi89@gmail.com>
2020-12-17 01:45:46 +0200

    class order

--- Ecommerce/Marketplace/models.py
+++ Ecommerce/Marketplace/models.py
@@ -16,4 +16,12 @@ class Seller(models.Model):
     address = models.CharField(max_length=200 ,null=True )
     credit_card_number =models.IntegerField()   
     def __str__ (self):
-        return self.user 
+        return self.user
+
+class Order(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
+    quantity=models.IntegerField(default=0,blank=True,null=True)
+    date=models.DateTimeField(auto_now_add=True)
+    checkout=models.ForeignKey(Checkout,on_delete=models.SET_NULL,blank=True,null=True)
+    def __str__ (self):
+        return self.checkout.id
