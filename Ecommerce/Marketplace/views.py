bb926c357d9b423c09a2d8e6c98e592cf0d7d931
omarelshamy12 <omarelshami99@gmail.com>
2020-12-18 16:26:11 +0200

    filling the database

--- Ecommerce/Marketplace/views.py
+++ Ecommerce/Marketplace/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from .models import *
 # Create your views here.
 def index (request):
-<<<<<<< HEAD
     return render(request ,'Marketplace/login.html' )
 
 def login (request):
