04cf0a97498a2957d5d1dacce1527b4e9fdb323a
omartarek206 <40853054+omartarek206@users.noreply.github.com>
2020-12-19 22:15:27 +0200

    add index.html

--- Ecommerce/Marketplace/views.py
+++ Ecommerce/Marketplace/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+
+
 # Create your views here.
-def index (request):
-    return render(request ,'Marketplace/index.html' ,
-    {
-        "products" : Product.objects.all() #product have  : name , id ,price ,seller ,image
+def index(request):
+    return render(request, 'Marketplace/index.html', {
+        "products": Product.objects.all()     # product has fields: name , id ,price ,seller ,image
     })
 
 
-def login (request):
-    return    render(request ,'Marketplace/login.html')
+def login(request):
+    return render(request, 'Marketplace/login.html')
 
-def register (request):
-    return render(request ,'Marketplace/register.html')
+
+def register(request):
+    return render(request, 'Marketplace/register.html')
