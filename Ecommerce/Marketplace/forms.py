bcbafee545a5db5453aa4a35e2dc82420feb886a
abdwd <abdelrhmandawoud@hotmail.com>
2020-12-20 17:24:47 +0200

    Telephone & Address are now added

--- Ecommerce/Marketplace/forms.py
+++ Ecommerce/Marketplace/forms.py
@@ -5,6 +5,9 @@ from django import forms
 from django.contrib.auth.models import User
 
 class CreateUserForm(UserCreationForm):
+    telephone = forms.CharField(max_length=200)
+    address = forms.CharField(max_length=200)
+
     class Meta:
         model= User
         fields=['username','email','password1','password2']
