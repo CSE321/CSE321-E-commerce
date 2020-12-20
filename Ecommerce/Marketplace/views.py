bc33bdd16caff7de41574e6fbee5f9ead9afddfd
karim-hafez77 <karimmagdi89@gmail.com>
2020-12-20 02:02:39 +0200

    i submitted login backend login

--- Ecommerce/Marketplace/views.py
+++ Ecommerce/Marketplace/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login
+
 from .models import *
+
 # Create your views here.
 def index (request):
     return render(request ,'Marketplace/index.html' ,
@@ -10,7 +14,24 @@ def index (request):
 
 
 def login (request):
-    return    render(request ,'Marketplace/login.html')
+
+    if request.method == "Post":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, 'Marketplace/login.html',{
+                "message":"wrong username or password"
+            })
+
+
+    return render(request, 'Marketplace/login.html')
+
+
+
 
 def register (request):
     return render(request ,'Marketplace/register.html')
