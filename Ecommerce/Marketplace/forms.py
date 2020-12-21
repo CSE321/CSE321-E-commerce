cdcefa796c9245b7574739e4dca7b8149d71639c
abdwd <abdelrhmandawoud@hotmail.com>
2020-12-22 00:15:02 +0200

    Register is now added

--- Ecommerce/Marketplace/forms.py
+++ Ecommerce/Marketplace/forms.py
@@ -2,13 +2,14 @@ from django.forms import ModelForm
 from .models import *
 from django.contrib.auth.forms import  UserCreationForm
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 
 class CreateUserForm(UserCreationForm):
     telephone = forms.CharField(max_length=200)
     address = forms.CharField(max_length=200)
-
+    credit_card_number=forms.CharField(max_length=200)
     class Meta:
         model= User
-        fields=['username','email','password1','password2']
+        fields=["username","email","password1","password2"]
 
--- Ecommerce/Marketplace/views.py
+++ Ecommerce/Marketplace/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login
-
+from django.contrib import messages
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import CreateUserForm
+from django.contrib.auth.forms import  UserCreationForm
+
 # Create your views here.
+type="Customer"
 def index(request):
     return render(request, 'Marketplace/index.html', {
         "products": Product.objects.all()     # product has fields: name , id ,price ,seller ,image
@@ -34,12 +38,19 @@ def login (request):
 
 
 def register(request):
-    form = CreateUserForm()
 
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
+            print(request.POST)
             form.save()
+            user_data = User.objects.get(username=form.cleaned_data["username"])
+            if(request.POST["usertype"]=="option2"):
+                seller =Seller(user=user_data ,email=request.POST["email"], address=request.POST["address"] ,credit_card_number=request.POST["credit_card_number"])
+                seller.save()
+            else:
+                customer =Customer(user=user_data ,email=request.POST["email"], address=request.POST["address"] ,credit_card_number=request.POST["credit_card_number"])
+                customer.save()
     context = {'form': form}
-
-    return render(request, 'Marketplace/register.html')
+    return render(request, 'Marketplace/register.html', context)
