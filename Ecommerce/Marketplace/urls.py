7c0df2a9b70597e861e1a7435fbfe5a5d2e2b384
omartarek206 <40853054+omartarek206@users.noreply.github.com>
2021-01-11 00:03:20 +0200

    Add product page

--- Ecommerce/Marketplace/urls.py
+++ Ecommerce/Marketplace/urls.py
@@ -3,10 +3,10 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns =[
-    path('' ,views.index ,name="index" ),
-    path('login' ,views.login ,name="login"),
-    path('register' ,views.register ,name="register"),
-    path('product/<int:id>',views.product ,name="product")
+    path('', views.index, name="index" ),
+    path('login', views.login, name="login"),
+    path('register', views.register, name="register"),
+    path('product/<int:id>', views.product, name="product")
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Ecommerce/Marketplace/views.py
+++ Ecommerce/Marketplace/views.py
@@ -14,27 +14,22 @@ def index(request):
     })
 
 
-
-
-def login (request):
+def login(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            auth_login(request,user)
+            auth_login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, 'Marketplace/login.html',{
-                "message":"wrong username or password"
+            return render(request, 'Marketplace/login.html', {
+                "message": "wrong username or password"
             })
     return render(request, 'Marketplace/login.html')
 
 
-
-
 def register(request):
-
     form = UserCreationForm()
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -42,23 +37,32 @@ def register(request):
             print(request.POST)
             form.save()
             user_data = User.objects.get(username=form.cleaned_data["username"])
-            if(request.POST["usertype"]=="option2"):
-                seller =Seller(user=user_data ,email=request.POST["email"], address=request.POST["address"] ,credit_card_number=request.POST["credit_card_number"])
+            if request.POST["usertype"] == "option2":
+                seller = Seller(user=user_data,
+                                email=request.POST["email"],
+                                address=request.POST["address"],
+                                credit_card_number=request.POST["credit_card_number"])
                 seller.save()
             else:
-                customer =Customer(user=user_data ,email=request.POST["email"], address=request.POST["address"] ,credit_card_number=request.POST["credit_card_number"])
+                customer = Customer(user=user_data,
+                                    email=request.POST["email"],
+                                    address=request.POST["address"],
+                                    credit_card_number=request.POST["credit_card_number"])
                 customer.save()
     context = {'form': form}
     return render(request, 'Marketplace/register.html', context)
 
-def product (request, id):
+
+# TODO save new reviews in the db
+def product(request, id):
+    if request.method == "POST":
+        review = request.POST["review"]
     product = Product.objects.get(id=id)
-    category =product.category
-    reviews = Review.objects.filter(product =id)
-    similar_products = Product.objects.filter(category = category).exclude(id=id)
-    return render(request ,'Marketplace/product.html',{ 
-        'product' :product ,
-        'reviews' : reviews ,
-        "similar_products" :similar_products 
+    category = product.category
+    reviews = Review.objects.filter(product=id)
+    similar_products = Product.objects.filter(category=category).exclude(id=id)
+    return render(request, 'Marketplace/product.html', {
+        'product': product,
+        'reviews': reviews,
+        "similar_products": similar_products
     })
-    
